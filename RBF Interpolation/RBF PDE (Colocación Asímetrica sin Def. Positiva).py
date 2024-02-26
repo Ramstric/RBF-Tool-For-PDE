5393f68dff814d140024460858ef30e1cfb91c75
progr <ramstric@gmail.com>
2024-02-25 23:19:35 -0600

    Polishing & more!

--- RBF Interpolation/RBF PDE (Colocación Asímetrica sin Def. Positiva).py
+++ RBF Interpolation/RBF PDE (Colocación Asímetrica sin Def. Positiva).py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from matplotlib import cm
-import matplotlib as mpl
+from mpl_toolkits.mplot3d import Axes3D
 
 #mpl.use('Qt5Agg')
 plt.rcParams['figure.figsize'] = (10, 10)
@@ -21,24 +21,17 @@ def D_kernel(S, epsilon):
     return arg_superior / pow(arg_bottom, 3/2)
 
 
-# Function of PDE
-def f(S):
-    return S[0]*np.exp(S[1])
+# Interior nodes
+x_I = np.arange(-1.6, 2, 0.4)
+# Boundary nodes
+x_B = np.array([-2, 2])
 
-
-# Make data.
+X = np.block([x_B, x_I])   # Making the Vector X (Boundary + Interior nodes)
 
 # Interior nodes
-x_I = np.arange(0.25, 2, 0.25)
+y_I = np.arange(-1.6, 2, 0.4)
 # Boundary nodes
-x_B = np.array([0, 2])
-
-X = np.block([x_B, x_I])                     # Making the Vector X (Boundary + Interior nodes)
-
-# Interior nodes
-y_I = np.arange(0.25, 1, 0.25)
-# Boundary nodes
-y_B = np.array([0, 1])
+y_B = np.array([-2, 2])
 
 Y = np.block([y_B, y_I])
 
@@ -46,23 +39,23 @@ Y = np.block([y_B, y_I])
 S = np.zeros(shape=(X.size * Y.size, 2))
 
 X, Y = np.meshgrid(X, Y)
-Z = X*np.exp(Y)
-#Z = np.sin(R)
-#- = np.ones(shape=(5, 9))
+#Z = X*np.exp(Y)
+Z = np.full(shape=(11, 11), fill_value=6, dtype=np.double)
+#Z = np.ones(shape=(5, 9))
 
 
 # Boundary conditions
 for row in range(y_I.size + y_B.size):
     for col in range(x_I.size + x_B.size):
-        if X[row, col] == 0:
-            Z[row, col] = 0
+        if X[row, col] == -2:
+            Z[row, col] = 5+2*pow(Y[row, col], 2)
         elif X[row, col] == 2:
-            Z[row, col] = 2*np.exp(Y[row, col])
+            Z[row, col] = 5+2*pow(Y[row, col], 2)
 
-        elif Y[row, col] == 0:
-            Z[row, col] = X[row, col]
-        elif Y[row, col] == 1:
-            Z[row, col] = np.exp(1)*X[row, col]
+        elif Y[row, col] == -2:
+            Z[row, col] = 9+pow(X[row, col], 2)
+        elif Y[row, col] == 2:
+            Z[row, col] = 9+pow(X[row, col], 2)
 
 zMatrix = np.stack(Z.ravel())
 
@@ -73,8 +66,8 @@ for idx in range(Z.size):
     S[idx, 1] = positions[1, idx]
 
 # Make allocation for RBF.
-X_RBF = np.arange(0, 2, 0.3333)
-Y_RBF = np.arange(0, 1, 0.2)
+X_RBF = np.arange(-2, 2, 0.2)
+Y_RBF = np.arange(-2, 2, 0.2)
 
 S_RBF = np.zeros(shape=(X_RBF.size * Y_RBF.size, 2))
 Z_RBF = np.zeros(shape=X_RBF.size * Y_RBF.size)
@@ -89,7 +82,7 @@ for idx in range(Z_RBF.size):
 
 # Variables
 size = Z.size
-b = 1                 # El valor de la base debe de elegirse de acuerdo al error de la interpolación
+b = 2                 # El valor de la base debe de elegirse de acuerdo al error de la interpolación
 
 # Inicio del metodo para RBF
 mainMatrix = np.zeros(shape=(size, size))
@@ -97,10 +90,10 @@ mainMatrix = np.zeros(shape=(size, size))
 for row in range(size):
     for col in range(size):
 
-        if S[row, 0] == 0 or S[row, 0] == 2:
+        if S[row, 0] == -2 or S[row, 0] == 2:
             mainMatrix[row, col] = kernel(S[row] - S[col], b)
 
-        elif S[row, 1] == 0 or S[row, 1] == 1:
+        elif S[row, 1] == -2 or S[row, 1] == 2:
             mainMatrix[row, col] = kernel(S[row] - S[col], b)
 
         else:
@@ -141,7 +134,10 @@ Z_RBF = np.stack(Z_RBF, axis=0)
 # Plot the surface
 ax = plt.axes(projection ="3d")
 
-surf = ax.plot_surface(X_RBF, Y_RBF, Z_RBF, cmap=cm.plasma, linewidth=0, antialiased=False, alpha=0.2, zorder=1)
+#ax.get_proj = lambda: np.dot(Axes3D.get_proj(ax), np.diag([1, 1, 2.5, 1]))
+
+
+surf = ax.plot_surface(X_RBF, Y_RBF, Z_RBF, cmap=cm.plasma, linewidth=0, antialiased=False, alpha=0.6, zorder=1)
 #scat = ax.scatter3D(X, Y, Z, s=25, zorder=4, c=Z, cmap=cm.plasma_r)
 #scat = ax.scatter3D(X_RBF, Y_RBF, Z_RBF, s=5, c="red")
 
