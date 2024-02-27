f6bd000e5c5ea515a2a2f91894e2a5e8cc455d31
progr <ramstric@gmail.com>
2024-02-26 23:24:05 -0600

    Ripple function on main!

--- main.py
+++ main.py
@@ -21,41 +21,49 @@ def D_kernel(S, epsilon):
     return arg_superior / pow(arg_bottom, 3/2)
 
 
+"""
+def D_kernel(S, epsilon):
+    arg_bottom = pow((epsilon) * np.linalg.norm(S), 2) + 1
+    arg_superior = pow(epsilon, 2) * (S[0] + S[1])
+    return arg_superior / np.sqrt(arg_bottom)
+"""
+
+# Must change NODES, RBD Points, Z matrix, Boundary cond both in value and if's
+
 # Interior nodes
-x_I = np.arange(-1.6, 2, 0.4)
+x_I = np.arange(-2.75, 3, 0.25)
 # Boundary nodes
-x_B = np.array([-2, 2])
+x_B = np.array([-3, 3])
 
 X = np.block([x_B, x_I])   # Making the Vector X (Boundary + Interior nodes)
 
 # Interior nodes
-y_I = np.arange(-1.6, 2, 0.4)
+y_I = np.arange(-2.75, 3, 0.25)
 # Boundary nodes
-y_B = np.array([-2, 2])
+y_B = np.array([-3, 3])
 
 Y = np.block([y_B, y_I])
-
 
 S = np.zeros(shape=(X.size * Y.size, 2))
 
 X, Y = np.meshgrid(X, Y)
-#Z = X*np.exp(Y)
-Z = np.full(shape=(11, 11), fill_value=6, dtype=np.double)
+Z = (- 4*pow(Y, 2) - 4*pow(X, 2)) * np.sin(pow(X, 2) + pow(Y, 2)) + 4 * np.cos(pow(X, 2) + pow(Y, 2))
+#Z = np.full(shape=(9, 9), fill_value=6, dtype=np.double)
 #Z = np.ones(shape=(5, 9))
 
 
 # Boundary conditions
 for row in range(y_I.size + y_B.size):
     for col in range(x_I.size + x_B.size):
-        if X[row, col] == -2:
-            Z[row, col] = 5+2*pow(Y[row, col], 2)
-        elif X[row, col] == 2:
-            Z[row, col] = 5+2*pow(Y[row, col], 2)
+        if X[row, col] == -3:
+            Z[row, col] = np.sin(9 + pow(Y[row, col], 2))
+        elif X[row, col] == 3:
+            Z[row, col] = np.sin(9 + pow(Y[row, col], 2))
 
-        elif Y[row, col] == -2:
-            Z[row, col] = 9+pow(X[row, col], 2)
-        elif Y[row, col] == 2:
-            Z[row, col] = 9+pow(X[row, col], 2)
+        elif Y[row, col] == -3:
+            Z[row, col] = np.sin(9 + pow(X[row, col], 2))
+        elif Y[row, col] == 3:
+            Z[row, col] = np.sin(9 + pow(X[row, col], 2))
 
 zMatrix = np.stack(Z.ravel())
 
@@ -66,8 +74,8 @@ for idx in range(Z.size):
     S[idx, 1] = positions[1, idx]
 
 # Make allocation for RBF.
-X_RBF = np.arange(-2, 2, 0.2)
-Y_RBF = np.arange(-2, 2, 0.2)
+X_RBF = np.arange(-3, 3, 0.05)
+Y_RBF = np.arange(-3, 3, 0.05)
 
 S_RBF = np.zeros(shape=(X_RBF.size * Y_RBF.size, 2))
 Z_RBF = np.zeros(shape=X_RBF.size * Y_RBF.size)
@@ -82,7 +90,7 @@ for idx in range(Z_RBF.size):
 
 # Variables
 size = Z.size
-b = 2                 # El valor de la base debe de elegirse de acuerdo al error de la interpolación
+b = 2                # El valor de la base debe de elegirse de acuerdo al error de la interpolación
 
 # Inicio del metodo para RBF
 mainMatrix = np.zeros(shape=(size, size))
@@ -90,10 +98,10 @@ mainMatrix = np.zeros(shape=(size, size))
 for row in range(size):
     for col in range(size):
 
-        if S[row, 0] == -2 or S[row, 0] == 2:
+        if S[row, 0] == -3 or S[row, 0] == 3:
             mainMatrix[row, col] = kernel(S[row] - S[col], b)
 
-        elif S[row, 1] == -2 or S[row, 1] == 2:
+        elif S[row, 1] == -3 or S[row, 1] == 3:
             mainMatrix[row, col] = kernel(S[row] - S[col], b)
 
         else:
@@ -124,8 +132,6 @@ def RBF(s):
 for idx in range(S_RBF.shape[0]):
     Z_RBF[idx] = RBF(S_RBF[idx])
 
-print(Z_RBF[8])
-
 Z_RBF = np.array_split(Z_RBF, X_RBF.shape[0])
 
 Z_RBF = np.stack(Z_RBF, axis=0)
@@ -134,10 +140,11 @@ Z_RBF = np.stack(Z_RBF, axis=0)
 # Plot the surface
 ax = plt.axes(projection ="3d")
 
-#ax.get_proj = lambda: np.dot(Axes3D.get_proj(ax), np.diag([1, 1, 2.5, 1]))
+ax.get_proj = lambda: np.dot(Axes3D.get_proj(ax), np.diag([1.25, 1.25, 0.1, 1]))
 
+ax.set_facecolor("slategrey")
 
-surf = ax.plot_surface(X_RBF, Y_RBF, Z_RBF, cmap=cm.plasma, linewidth=0, antialiased=False, alpha=0.6, zorder=1)
+surf = ax.plot_surface(X_RBF, Y_RBF, Z_RBF, cmap=cm.Blues_r, linewidth=0, antialiased=False, alpha=1, zorder=1)
 #scat = ax.scatter3D(X, Y, Z, s=25, zorder=4, c=Z, cmap=cm.plasma_r)
 #scat = ax.scatter3D(X_RBF, Y_RBF, Z_RBF, s=5, c="red")
 
